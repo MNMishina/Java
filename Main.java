13dc69f1b34b5ca722d3e0e202dc9c453ea49450
MNMishina <ninja5575@gmail.com>
2021-07-27 17:18:04 +0300

    TaskWork01 commit

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/*
+Задача №1 - Первая программа
+Вывести на консоль текст “Hello World!”:
+Скачать и установить IntelliJ IDEA.
+Написать первую программу, которая выводит в консоль строку “Hello World!”.
+Подключиться к GitHub, залить программу в созданный репозиторий.
+ */
+
 package com.max.idea;
 public class Main {
     public static void main(String[] args) {
