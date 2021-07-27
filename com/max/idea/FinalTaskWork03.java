f42152b10652a8676a9d4d882a67184d7b35edfd
MNMishina <ninja5575@gmail.com>
2021-07-27 16:58:00 +0300

    TaskWork05 commit

---
+++ com/max/idea/FinalTaskWork03.java
@@ -0,0 +1,21 @@
+/*
+Итоговая задача № 3
+Напишите программу, где пользователь вводит сначала количество строк n, затем сами строки. Среди данных строк найти строку с максимальным количеством различных символов. Если таких строк будет много, то вывести первую.
+Пример для теста работы программы:
+Количество строк: 3
+Строка 1: привет
+Строка 2: анализ
+Строка 3: 111111111111
+Ответ: привет
+
+package com.max.idea;
+import java.util.Scanner;
+public class FinalTaskWork03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ввод: ");
+        String str = scanner.nextLine();
+        scanner.close();
+}
+
+*/
